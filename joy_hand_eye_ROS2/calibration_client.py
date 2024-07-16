1210f927edcfe9a058a2d7e9c0e69c2457655132
Irving Fang <zichuanfang2015@yahoo.com>
2024-07-16 12:04:04 -0400

    small refactor, more comments

--- joy_hand_eye_ROS2/calibration_client.py
+++ joy_hand_eye_ROS2/calibration_client.py
@@ -7,16 +7,23 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
 
-class FrameListener(Node):
-
+class EEFPoseListener(Node):
+    '''
+    The only purpose of this class is to listen to the TF and get the transform between the base and the end effector.
+    Making it an independent node so it can be easily multi-threaded.
+    '''
     def __init__(self):
         super().__init__('turtle_tf2_frame_listener')  # type: ignore
 
+        # buffer to hold the transform in a cache
         self.tf_buffer = Buffer()
+
+        # listener. Important to spin a thread, otherwise the listen will block and no TF can be updated
         self.tf_listener = TransformListener(buffer=self.tf_buffer, node=self, spin_thread=True)
     
     def get_transform(self, target_frame, source_frame):
         try:
+            # Timeout is important so the TF can propely initialize instead of blocking the program with None indefinitely
             transform = self.tf_buffer.lookup_transform(target_frame, source_frame, Time(), timeout=Duration(seconds=2))
             return transform
         except Exception as e:
@@ -25,11 +32,10 @@ class FrameListener(Node):
 
 def main():
     rclpy.init()
-    node = FrameListener()
-    transform = node.get_transform('link_base', 'link_eef')
-    if transform:
-        node.get_logger().info('Transform: %s' % transform)
-    rclpy.spin(node)
+    eef_pose_node = EEFPoseListener()
+    transform = eef_pose_node.get_transform('link_base', 'link_eef')
+
+    rclpy.spin(eef_pose_node)
     rclpy.shutdown()
     
 
