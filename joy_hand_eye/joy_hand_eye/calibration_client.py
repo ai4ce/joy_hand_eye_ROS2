c659d42e2f0867c39919ff0a86e8d1d4d738c618
Irving Fang <zichuanfang2015@yahoo.com>
2024-07-21 14:23:05 -0400

    all code functionality done. Need to test it on the hardware. Need to add a few tunable parameters

--- joy_hand_eye/joy_hand_eye/calibration_client.py
+++ joy_hand_eye/joy_hand_eye/calibration_client.py
@@ -1,19 +1,11 @@
-from gc import garbage
 import rclpy
 from rclpy.node import Node
-from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
+from rclpy.executors import MultiThreadedExecutor
 
-from joy_hand_eye_msg.srv import CollectPoses
+from sensor_msgs.msg import Joy
 from std_srvs.srv import Empty
 
-
-from usbcam_capture.usbcam_image_client import USBCamImageClient
-
-import cv2
-from cv_bridge import CvBridge
-
 from queue import Queue
-import os
 
 
 class CalibrationClient(Node):
@@ -23,20 +15,33 @@ class CalibrationClient(Node):
 
 
         ############################ Miscanellous Setup #######################################
-        self.cvbridge = CvBridge() # for converting ROS images to OpenCV images
 
         self._debounce_setup() # for debouncing the capture button
-        self.shutter = False # when this is true, the client will issue a request to the server to capture images
+        self.shutter = False # when this is true, the client will issue a request to the server to capture run hand eye calibration
 
         ############################ Client Setup #############################################
         # Pose collecting client
-        # self.collect_pose_cli = self.create_client(
-        #     srv_type=CollectPoses, 
-        #     srv_name='/joy_hand_eye/collect_poses')
-        # while not self.collect_pose_cli.wait_for_service(timeout_sec=1.0):
-        #     self.get_logger().info('collect poses service not available, waiting again...')
+        self.hand_eye_cli = self.create_client(
+            srv_type=Empty, 
+            srv_name='/joy_hand_eye/run_hand_eye_calibration')
+        while not self.hand_eye_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('collect poses service not available, waiting again...')
         
-        # self.pose_req = CollectPoses.Request()
+        self.hand_eye_req = Empty.Request()
+
+        ############################ Subscriber Setup #########################################
+        # subscribe to joy topic to read joystick button press
+        self.joy_sub = self.create_subscription(
+            msg_type=Joy, 
+            topic='/joy', 
+            callback=self.joy_callback, 
+            qos_profile=10)
+
+    def joy_callback(self, msg):
+        old_value = self.debounce_buffer.get() # pop the oldest read
+        self.debounce_buffer.put(msg.buttons[10]) # push the newest read
+        if old_value == 0 and msg.buttons[10] == 1: # button 10 is the PS button
+            self.shutter = True # rising edge detected
     
     def _debounce_setup(self):
         '''
@@ -52,45 +57,30 @@ class CalibrationClient(Node):
 
 def main():
 
-    # construct the charuco board.
-
     rclpy.init()
     executor = MultiThreadedExecutor()
-
-    # image_node = USBCamImageClient()
-    # executor.add_node(image_node)
 
     client = CalibrationClient()
     executor.add_node(client)
     
 
     while rclpy.ok():
-        # if image_node.shutter: # shutter down
+        if client.shutter: # shutter down
 
-        #     # send request to server to capture images
-        #     image_future = image_node.image_cli.call_async(image_node.image_req)
+            # send request to server to capture images
+            hand_eye_future = client.hand_eye_cli.call_async(client.hand_eye_req)
 
-        #     # immediately shutter up to debounce, so we don't caputre multiple images
-        #     image_node.shutter = False
+            # immediately shutter up to debounce, so we don't caputre multiple images
+            client.shutter = False
             
-        #     # wait for the server to capture images
-        #     rclpy.spin_until_future_complete(image_node, image_future)
-
-        #     # get the images from the server
-        #     image_response = image_future.result()
-
-        #     # postprocess the image and get it
-        #     image = image_node.postprocess(image_response.frame)
-        #     # ros_image = image_node.cvbridge.cv2_to_imgmsg(image, encoding='bgr8')
-
-        #     # client.pose_req.frame = ros_image
-        #     # pose_future = client.collect_pose_cli.call_async(client.pose_req)
+            # wait for the server to capture images
+            rclpy.spin_until_future_complete(client, hand_eye_future)
             
-        #     client.get_logger().info('Pose collected...')
+            client.get_logger().info('Hand eye calibration done! You can now close the program.')
             
         # client.get_logger().info(f'The shutter is {image_node.shutter}')
         executor.spin_once()
 
-    # image_node.destroy_node()
+
     client.destroy_node()
     rclpy.shutdown()
